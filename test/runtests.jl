cb817d5a11b6f5a311994f0edb0e2de5f44af2ec
pkuyyj <yuejyu@student.ethz.ch>
2023-12-22 04:01:52 +0100

    add CI/CD; add test; add literature

---
+++ test/runtests.jl
@@ -0,0 +1,15 @@
+using Test
+using PorousConvection
+
+function runtests()
+    exename = joinpath(Sys.BINDIR, Base.julia_exename())
+    testdir = pwd()
+
+    printstyled("Testing Elastic Wave 3D.jl\n"; bold=true, color=:white)
+
+    run(`$exename -O3 --startup-file=no $(joinpath(testdir, "test3D.jl"))`)
+
+    return 0
+end
+
+exit(runtests())
